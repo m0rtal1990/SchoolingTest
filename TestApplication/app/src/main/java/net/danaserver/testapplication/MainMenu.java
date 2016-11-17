9cad4fce9c9f7277ea4f4989d83603cdfcef1f3e
LIVING-DESKTOP\Richard J. Dana <richard_dana@live.com>
2016-11-17 12:50:17 +0100

    no message

--- TestApplication/app/src/main/java/net/danaserver/testapplication/MainMenu.java
+++ TestApplication/app/src/main/java/net/danaserver/testapplication/MainMenu.java
@@ -2,12 +2,17 @@ package net.danaserver.testapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
+    private Button callClickListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        // Setting Call Click Listener for the main menu
+        
     }
 }
